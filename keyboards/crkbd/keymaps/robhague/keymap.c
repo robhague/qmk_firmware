54bd4c75bd65a141c66d1772b9900d29b94f44fc
Rob Hague <rob@rho.org.uk>
2022-07-23 15:38:24 +0100

    Add mousekeys

--- keyboards/crkbd/keymaps/robhague/keymap.c
+++ keyboards/crkbd/keymaps/robhague/keymap.c
@@ -31,13 +31,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      LCMD(LCTL(KC_Q)),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O, XXXXXXX, MO(3),
+      LCMD(LCTL(KC_Q)),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O, XXXXXXX, MO(4),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_ESC, LOPT_T(KC_A), LCTL_T(KC_S), LCMD_T(KC_D), LSFT_T(KC_F), KC_G,      KC_H, RSFT_T(KC_J), RCMD_T(KC_K), RCTL_T(KC_L), ROPT_T(KC_P), XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                     XXXXXXX, XXXXXXX, LT(1, KC_SPC),   LT(2, KC_BSPC), XXXXXXX, XXXXXXX
+                                     XXXXXXX, MO(3), LT(1, KC_SPC),   LT(2, KC_BSPC), MO(3), XXXXXXX
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -69,7 +69,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
+  // Mouse
   [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      XXXXXXX, XXXXXXX, KC_MS_BTN2, KC_MS_UP, KC_MS_BTN1, KC_MS_WH_UP,                      KC_MS_WH_UP, KC_MS_BTN3, KC_MS_UP, KC_MS_BTN2, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX,                      XXXXXXX, KC_MS_LEFT, KC_MS_BTN1, KC_MS_RIGHT, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_BTN3, XXXXXXX, KC_MS_WH_DOWN,                      KC_MS_WH_DOWN, XXXXXXX, KC_MS_DOWN, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          _______, _______,  _______,     _______, _______, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  // Control
+  [4] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
