818babacd31843177e17c7cec9894fd53a2b62bf
Rob Hague <rob@rho.org.uk>
2022-07-23 15:09:35 +0100

    Reduce tapping term

--- keyboards/crkbd/keymaps/robhague/config.h
+++ keyboards/crkbd/keymaps/robhague/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 300
+#define TAPPING_TERM 150
 #define IGNORE_MOD_TAP_INTERRUPT
 
 //define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
